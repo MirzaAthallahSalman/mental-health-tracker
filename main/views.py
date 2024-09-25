9d4aa476e00911600730922865eed55cf8e70582
Mirza Athallah Salman <atar.haryanto@gmail.com>
2024-09-25 13:28:52 +0700

    tutorial 4 done

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import reverse
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -17,7 +18,7 @@ def show_main(request):
     mood_entries = MoodEntry.objects.filter(user=request.user)
     context = {
         'name': request.user.username,
-        'class': 'PBP D',
+        'class': 'PBP A',
         'npm': '2306123456',
         'mood_entries': mood_entries,
         'last_login': request.COOKIES['last_login'],
@@ -86,3 +87,26 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_mood(request, id):
+    # Get mood entry berdasarkan id
+    mood = MoodEntry.objects.get(pk = id)
+
+    # Set mood entry sebagai instance dari form
+    form = MoodEntryForm(request.POST or None, instance=mood)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_mood.html", context)
+
+def delete_mood(request, id):
+    # Get mood berdasarkan id
+    mood = MoodEntry.objects.get(pk = id)
+    # Hapus mood
+    mood.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
